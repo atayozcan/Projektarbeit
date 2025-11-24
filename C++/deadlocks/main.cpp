81c237d4ac1e9b0e9fc34241a965c1f7a25570c4
daniel <weingand.daniel@gmail.com>
2025-11-24 13:48:35 +0100

    deadlock c++

--- C++/deadlocks/main.cpp
+++ C++/deadlocks/main.cpp
@@ -1,8 +1,34 @@
-#include "main.h"
+// 10_deadlock_01.cpp
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
 
-#include <cstdio>
-#include <cstdlib>
+mutex resource1;
+mutex resource2;
+
+void threadFunction1() {
+    lock_guard lock1(resource1);
+    cout << "Thread 1 has first lock" << endl;
+    this_thread::sleep_for(chrono::milliseconds(100)); // Simulating work
+    lock_guard lock2(resource2); // This will wait for resource2
+    cout << "Thread 1 has second lock" << endl;
+}
+
+void threadFunction2() {
+    lock_guard lock2(resource2);
+    cout << "Thread 2 has second lock" << endl;
+    this_thread::sleep_for(chrono::milliseconds(100)); // Simulating work
+    lock_guard lock1(resource1); // This will wait for resource1
+    cout << "Thread 2 has first lock" << endl;
+}
 
 int main() {
-    return EXIT_SUCCESS;
+    thread t1(threadFunction1);
+    thread t2(threadFunction2);
+
+    t1.join();
+    t2.join();
+
+    return 0;
 }
